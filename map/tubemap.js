cf3855cbe53623fe382f267612147a1b15805007
Chris Horn <chrishornuk@gmail.com>
2019-01-31 18:33:39 +0000

    working properly now

--- map/tubemap.js
+++ map/tubemap.js
@@ -1,5 +1,3 @@
-
-
 function TubeMap(cols, rows, x, y, w, h, allowDiagonals, wallRatio) {
     // How many columns and rows?
     this.cols = cols;
@@ -15,67 +13,51 @@ function TubeMap(cols, rows, x, y, w, h, allowDiagonals, wallRatio) {
     this.w = w;
     this.h = h;
 
-    var line1End = Math.round(rows / 3);
-    var line2End = Math.round(cols / 2);
+    var line1End = Math.round(cols / 2);
+    var line2End = Math.round(rows / 2);
 
     //console.log(line1End);
     //console.log(line2End);
 
     var points = [
-        { from: [0, 0], to: [line1End, 0] },
-        { from: [line1End, 0], to: [line1End, line2End] },
-        { from: [line1End, line2End], to: [rows-1, line2End] },
-        { from: [rows-1, line2End], to: [rows-1, cols-1] }
+        { from: [0, 0], to: [0, line1End] },
+        { from: [0,line1End], to: [line2End, line1End] },
+        { from: [line2End, line1End], to: [line2End, rows-1] },
+        { from: [line2End, rows-1], to: [cols-1, rows-1] }
     ];
+
+    console.log("points:");
+    console.log(points);
 
     // Making a 2D array
     for (var i = 0; i < cols; i++) {
         this.grid[i] = [];
         this.map[i] = [];
     }
-//debugger;
+
     for (var i = 0; i < points.length; i++) {
-        //console.log("points:" + i);
         if (points[i].from[1] == points[i].to[1]) {
-            console.log("first");
             for (var j = points[i].from[0]; j <= points[i].to[0]; j++) {
-                x = points[i].from[1]
-                this.map[x][j] = true;
-            //    console.log("x="+x+","+"j="+j);
+                var r = points[i].from[1]
+                this.map[j][r] = true;
             }
         } else if (points[i].from[0] == points[i].to[0]) {
-            console.log("second");
             for (var j = points[i].from[1]; j <= points[i].to[1]; j++) {
-                y = points[i].from[0]
-                this.map[j][y] = true;
-            //    console.log("j="+j+","+"y="+y);
+                var c = points[i].from[0]
+                this.map[c][j] = true;
             }
         }
     }
 
     var isWall;
-    for (var i = 0; i < rows; i++) {
-        for (var j = 0; j < cols; j++) {
-            isWall = false;
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
             if (!this.map[i][j]) {
                 isWall = true;
             } else {
-                console.log("map["+i+"]["+j+"]"+this.map[i][j]);
+                isWall = false;
             }
             this.grid[i][j] = new Spot(i, j, x + i * w / cols, y + j * h / rows, w / cols, h / rows, isWall, this.grid);
         }
     }
-
-    // for (var i = 0; i < cols; i++) {
-    //     for (var j = 0; j < rows; j++) {
-    //         var isWall = random(1.0) < wallRatio;
-    //         this.grid[i][j] = new Spot(i, j, x + i * w / cols, y + j * h / rows, w / cols, h / rows, isWall, this.grid);
-    //     }
-    // }
-
-    //for (var i = 0; i < cols; i++) {
-    //    for (var j = 0; j < rows; j++) {
-    //        this.grid[i][j].addNeighbors(this.grid, allowDiagonals);
-    //    }
-    //}
 }
